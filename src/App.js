693381d565761bd7eff34243b78c0b1c38766eff
dmittal1 <mittald@hotmail.co.uk>
2019-11-28 16:51:08 +0000

    Changes Made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -24,11 +26,35 @@ class App extends Component {
     ]
   }
 
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  // Add Todo
+  addTodo = (title) => {
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
   render(){
     console.log(this.state.todos);
   return (
     <div className="App">
-      <Todos todos={this.state.todos}/>
+      <div className="container">
+        <Header />
+        <AddTodo addTodo={this.addTodo} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} 
+          delTodo={this.delTodo}/>
+      </div>
     </div>
   );
 }
